90e04f1808fe5b27077b68234125fc946adbdc70
Ruben Rodriguez <ruben.rodriguez@megaterios.co>
2020-04-08 17:40:38 -0500

    feat: Forgot interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'users/login',
     component: LoginComponent
+  },
+  {
+    path: 'users/forgot',
+    component: ForgotComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    ForgotComponent,
   ],
   imports: [
     BrowserModule,
