5c278588bfdd9e1c3ca183348f87f015b8ce7e02
Ruben Rodriguez <ruben.rodriguez@megaterios.co>
2020-04-27 20:58:19 -0500

    feat: Add alert messages

--- src/app/components/dependencies-add/dependencies-add.component.ts
+++ src/app/components/dependencies-add/dependencies-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import Swal from "sweetalert2";
 
 //Interfaces
 import { Dependencie } from "../../interfaces/Dependencie";
@@ -27,7 +28,11 @@ export class DependenciesAddComponent implements OnInit {
 
   createDependencie(): void {
     if (this.dependencie.name == '') {
-      console.log("Debe llenar el campo");
+      Swal.fire({
+        icon: 'warning',
+        text: 'Debe llenar todos los campos',
+        confirmButtonColor: '#00aa99'
+      });
     } else {
       if (this.dependencie.name.length < 100) {
         this.dependenciesService.createDependencie(this.dependencie)
@@ -35,11 +40,21 @@ export class DependenciesAddComponent implements OnInit {
             res => {
               console.log(res);
               this.router.navigate(['/dependencies']);
+              Swal.fire({
+                title: 'Hecho',
+                text: 'La dependencia se creó con exito',
+                icon: 'success',
+                confirmButtonColor: '#00aa99'
+              });
             },
             err => console.error(err)
           )
       }else{
-        console.log("Máximo 100 caracteres");
+        Swal.fire({
+          icon: 'warning',
+          text: 'Solo se permite máximo 100 caracteres',
+          confirmButtonColor: '#00aa99'
+        });
       }
     }
   }
